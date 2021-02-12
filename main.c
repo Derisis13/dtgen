ac6caf991570e8b6ebdbf42632f62d407e1516d4
Derisis13 <66648475+Derisis13@users.noreply.github.com>
2021-02-13 00:12:51 +0100

    Add files via upload
    Added feature: string appending
    From now on you can use the -a option followed by the string you want to append to the desktop entry

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 
 void help(){
     printf("options:\n "
+           "-a [append] append any other string at the end of the file (eg. Terminal=true)"
            "-C [category] add to category, default is none (multiple categories can be separated with a semicolon)\n "
            "-c [comment] add comment, default is none\n "
            "-e [executable] specify executable, mandatory, (check desktop entry documentation for details: https://www.freedesktop.org/wiki/Specifications/desktop-entry-spec/)\n "
@@ -26,27 +27,25 @@ int main(int argc, char* argv[]) {
         printf("usage: \"dtgen -n name -e executable\" or \"dtgen -h\" for help\n");
         return 0;
     }
-    char *blank="", *comment=blank, *category=blank, *executable=NULL, *icon=blank, *name=NULL;
+    char *blank="", *comment=blank, *category=blank, *executable=NULL, *icon=blank, *name=NULL, *appendix=blank;
     for (int i = 1; i < argc; ++i) {
         if (strcmp(argv[i], "-c")==0) {
             comment = argv[++i];
-            continue;
         }
-        if (strcmp(argv[i], "-C")==0) {
+        else if (strcmp(argv[i], "-C")==0) {
             category = argv[++i];
-            continue;
         }
-        if (strcmp(argv[i], "-e")==0) {
+        else if (strcmp(argv[i], "-e")==0) {
             executable = argv[++i];
-            continue;
         }
-        if (strcmp(argv[i], "-i")==0) {
+        else if (strcmp(argv[i], "-i")==0) {
             icon = argv[++i];
-            continue;
         }
-        if (strcmp(argv[i], "-n")==0) {
+        else if (strcmp(argv[i], "-n")==0) {
             name = argv[++i];
-            continue;
+        }
+        else if (strcmp(argv[i], "-a")==0) {
+            appendix = argv[++i];
         }
         else {
             printf("unexpected option: %s\n", argv[i]);
@@ -63,6 +62,7 @@ int main(int argc, char* argv[]) {
            "Comment=%s\n"
            "Exec=%s\n"
            "Icon=%s\n"
-           "Categories=%s\n" ,name ,comment ,executable, icon, category);
+           "Categories=%s\n"
+           "%s\n" ,name ,comment ,executable, icon, category, appendix);
     return 0;
 }
